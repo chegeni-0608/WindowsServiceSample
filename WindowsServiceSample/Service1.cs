6869b1126ae3190ce77b35b56e9df7046ecc0878
chegeni-0608 <mmoa80610@gmail.com>
2024-09-07 11:27:40 +0330

    Add project files.

---
+++ WindowsServiceSample/Service1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceSample
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log("Windows Service Start!");
+
+            Timer timer = new Timer();
+            timer.Interval = 5000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Log("imer_Elapsed");
+        }
+
+        protected override void OnStop()
+        {
+            Log("Windows Service Stop!");
+        }
+
+        private void Log(string message)
+        {
+            string path = @"C:\Users\chegeni\Desktop\C#Projects\Log\log.txt";
+            System.IO.File.AppendAllLines(path, new List<string> { $"{message} + {DateTime.Now}"});
+
+        }
+    }
+}
